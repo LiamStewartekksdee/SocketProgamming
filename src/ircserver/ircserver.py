d1eaa36c55b750f7a70b38bb1d7394937ab166a1
Youssless <yaseenlahmami98@gmail.com>
2019-12-01 21:17:35 +0000

    updating Yaseen branch to latest commits

--- src/ircserver/ircserver.py
+++ src/ircserver/ircserver.py
@@ -5,80 +5,96 @@
 import socket
 import selectors
 import types
-import threading
-from queue import Queue
+import channel
+import client
 
 
-
-class Server():
+class Server(object):
     def __init__(self):
-        self.NUM_THREADS = 2
-        self.JOB_NUM = [1, 2, 3]  
-        self.queue = Queue()
         self.channels = {}
-        self.threads = []
-        self.registers = []
-
-        self.HOST = 'localhost'
+        self.clients = {}
+        self.HOST = '127.0.0.1'
         self.PORT = 6667
         self.sel = selectors.DefaultSelector()
 
-        self.create_threads()
-        self.init_jobs()
-        #self.server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #self.channels['localhost'] = self.server_sock
+    def start(self):
+        # sel = selectors.DefaultSelector()
+        # # define server address
+        # HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+        # # Port to listen on (non-privileged ports are > 1023)
+        # PORT = 6667
 
-        
-
-    def start(self, sock, event_type, HOST, PORT):
-        sock.bind((HOST, PORT))
-        sock.listen(10)
-        print('listening on', (HOST, PORT))
+        lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lsock.bind((self.HOST, self.PORT))
+        lsock.listen()
+        print('listening on', (self.HOST, self.PORT))
         # don't block when using socket, as we select from multiple sockets using selector
-        sock.setblocking(False)
-        self.registers.append(self.sel.register(sock, event_type, data=None))
+        lsock.setblocking(False)
+        self.sel.register(lsock, selectors.EVENT_READ, data=None)
 
-        for key, mask in self.sel.select(timeout=None):
-            print('key: {0}, mask: {1}'.format(key, mask))
-
-    def create_sock(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        return sock
-
-    def accept_conn(self):
+    def run(self):
         while True:
+            # readlist, writelist, emptylist = select.select()
             events = self.sel.select(timeout=None)
             for key, mask in events:
-                #print('{0}, {1}'.format(key, mask))
                 # if new connection, accept it using our wrapper function to create socket object and register it with the selector
                 if key.data is None:
                     self.accept_wrapper(key.fileobj)
                 # if it's been accepted, read or write or close
                 else:
                     self.service_connection(key, mask)
-            
+
     def accept_wrapper(self, sock):
-        print('Waiting for connection to the client ... ')
         conn, addr = sock.accept()  # Should be ready to read
+        print(conn)
         print('accepted connection from', addr)
         conn.setblocking(False)
         data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
         self.sel.register(conn, events, data=data)
+        # create a Client object and hold it in a dictionary as a pair socket : Client object
+        self.clients[conn] = client.Client(self, conn)
+        print("Accepted connection from %s:%s." % (addr[0], addr[1]))
+    
+    def __parse_input(self, line):
+        x = line.split()
+        command = x[0]
+        arguments = x[1:]
+        # new_list = []
+        # for x in arguments:
+            # x = str(x, 'utf-8')
+            # new_list.append(x)
+        return command, arguments
 
     def service_connection(self, key, mask):
         sock = key.fileobj
         data = key.data
         if mask & selectors.EVENT_READ:
-            recv_data = sock.recv(1024)  # Should be ready to read
-            print(recv_data)
+            recv_data = sock.recv(1024) # Should be ready to read
             if recv_data:
                 data.outb += recv_data
-
-            #     command, arguments = __parse_input(recv_data)
-            #     if(command == "JOIN" & len(arguments)>0) joinchannel(,argument[0])
+                # parse the line provided by the user
+                command, arguments = self.__parse_input(recv_data.decode('utf-8'))
+                # if found 'join' command
+                print("received data:")
+                print(command)
+                print(arguments)
+                if((command.upper() == "JOIN") and (len(arguments)>0)): 
+                    # find the client in the dict searching for his socket
+                    print("found join command and argument")
+                    if sock in self.clients:
+                        print("found client in dictionary")
+                        # get client object that is in our dictionary as a 
+                        # pair socket : Client object and connect Client to channel
+                        print(arguments[0])
+                        self.clients[sock].join_channel(arguments[0])
             else:
-                print('closing connection to ', data.addr)
+                print('closing connection to', data.addr)
+                # leave channel before deleting socket
+                self.clients[sock].leave_channel(sock)
+                # delete client from the dictionary
+                if sock in self.clients: del self.clients[sock]
+                # unregister and close socket made for a client
                 self.sel.unregister(sock)
                 sock.close()
         if mask & selectors.EVENT_WRITE:
@@ -87,70 +103,23 @@ class Server():
                 sent = sock.send(data.outb)  # Should be ready to write
                 data.outb = data.outb[sent:]
 
-                # if repr(data.outb).find('nJOIN #testchannel'):
-                #     if '#testchannel' not in self.channels:
-                #         print('creating test channel...')
-                #         sock = self.create_sock()
-                #         self.channels['#testchannel'] = sock
-                #         self.start(sock, selectors.EVENT_READ, self.HOST, self.PORT+1)
-                
+    def get_channel(self, channelname):
+        if channelname in self.channels:
+            channel1 = self.channels[channelname.lower()]
+        else:
+            channel1 = channel.Channel(self, channelname)
+            print(channelname + " channel created")
+            self.channels[channelname.lower()] = channel1
+        return channel1
+    
+    def delete_channel(self, channel):
+        del self.channels[channel.lower()]
 
-    def run(self):
-        while True:
-            j_num = self.queue.get()
-            if j_num == 1:
-                self.start(self.create_sock(), selectors.EVENT_READ, self.HOST, self.PORT)
-                self.accept_conn()
-            # if j_num == 2:
-            #     self.start(self.create_sock(), selectors.EVENT_READ, self.HOST, 6668)
-            
-            # if j_num == 3:
-            #     self.accept_conn()  
+    def remove_member_from_channel(self, client, channelname):
+        if channelname.lower() in self.channels:
+            channel = self.channels[channelname.lower()]
+            channel.remove_member(client)
 
-                #self.accept_conn()
-
-            
-            self.queue.task_done()
-
-    def create_threads(self):
-        for i in range(self.NUM_THREADS):
-            t = threading.Thread(target=self.run)
-            t.daemon = True
-            t.start()
-            self.threads.append(t)
-                
-    def init_jobs(self):
-        for i in self.JOB_NUM:
-            self.queue.put(i)
-
-        self.queue.join()      
-
-if __name__ == '__main__':
-    ircserver = Server()
-
-    # #simple server
-    # HOST = 'localhost'
-    # PORT = 7000
-    # sel = selectors.DefaultSelector()
-
-    # serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # serv_socket.bind((HOST, PORT))
-    # serv_socket.listen(10)
-    # serv_socket.setblocking(False)
-
-    # sel.register(serv_socket, selectors.EVENT_READ | selectors.EVENT_WRITE)
-
-    # while True:
-    #     for key, mask in sel.select(timeout=1):
-    #         conn = key.fileobj
-    #         print('client {}'.format(conn.getpeername()))
-
-    #         if mask and selectors.EVENT_READ:
-    #             print('ready to read')
-    #             data = conn.recv(512)
-    #             if data:
-    #                 print('recieved {!r}'.format(data))
-            
-    #         if mask and selectors.EVENT_WRITE:
-    #             print('ready to write')
-
+server = Server()
+server.start()
+server.run()
